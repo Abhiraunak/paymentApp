52313b318e03c5a42f1f40d148c4c2361dce5224
Abhishek <abhishekkp1721@hotmail.com>
2025-01-06 17:35:52 +0530

    add transaction logic in bank_dummy_server

--- apps/bank_webhook_handler/src/index.ts
+++ apps/bank_webhook_handler/src/index.ts
@@ -2,11 +2,53 @@ import express from "express";
 import db from "@repo/db/client";
 
 const app = express();
+app.use(express.json());
 
-app.post('/hdfcWebhook', (req, res) => {
-    const paymentInformation = {
-        token  : req.body.token,
-        userId : req.body.user_identifier,
-        amount : req.body.amount
+app.post('/hdfcWebhook', async (req, res) => {
+    // we can perfome zod validation here
+    const paymentInformation: {
+        token: string;
+        userId: string;
+        amount: string;
+    } = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
     };
+
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+
+            db.onRampTransaction.updateMany({
+                where: {
+                    token: paymentInformation.token
+                },
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.status(200).json({
+            message: "success"
+        })
+
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({
+            message: "Error while processing webhook"
+        })
+    }
 })
+
+app.listen(3003)
