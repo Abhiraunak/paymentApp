9067e7e7a86de7327e4fc1f834784cf864bd5fa1
Abhishek <abhishekkp1721@hotmail.com>
2025-01-07 15:49:41 +0530

    add a generic appbar

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./provider";
 import { Inter } from "next/font/google";
+import { AppbarClient } from "./AppbarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
+        <AppbarClient />
         <body className={inter.className}>{children}</body>
       </Providers>
     </html>
